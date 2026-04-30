dabb0409da9972964a9fd013b774864080b193e7
Fan DANG <i@dang.fan>
2026-04-30 10:09:35 +0800

    Log credMgmt random state for repro

--- tests/standard/fido2v1/test_credmgmt.py
+++ tests/standard/fido2v1/test_credmgmt.py
@@ -1,3 +1,6 @@
+import base64
+import os
+import pickle
 import pytest
 import time
 import random
@@ -10,6 +13,17 @@ from tests.utils import *
 from binascii import hexlify
 
 PIN = "123456"
+
+
+def _encode_random_state():
+    return base64.b64encode(pickle.dumps(random.getstate())).decode("ascii")
+
+
+def _restore_random_state_from_env():
+    state_b64 = os.environ.get("CANOKEY_TEST_RANDOM_STATE_B64")
+    if state_b64:
+        random.setstate(pickle.loads(base64.b64decode(state_b64)))
+        print("RESTORED_RANDOM_STATE_B64:", state_b64)
 
 
 @pytest.fixture(params=[PIN], scope = 'function')
@@ -271,6 +285,8 @@ class TestCredentialManagement(object):
         reason="Takes too much time on real hardware",
     )
     def test_interleaved_add_delete(self, device, PinToken, CredMgmt):
+        _restore_random_state_from_env()
+        print("INTERLEAVED_RANDOM_STATE_B64:", _encode_random_state())
         RPs = [{"id": "new_rp1.com"}, {"id": "new_rp2.com"}, {"id": "new_rp3.com"}]
         reg = None
         regs = {}
@@ -670,5 +686,4 @@ class TestCredentialManagement(object):
         with pytest.raises(CtapError) as e:
             cmd(credMgmt)
         assert e.value.code == CtapError.ERR.PIN_BLOCKED
-
 
